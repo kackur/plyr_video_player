7598d1913f6f691b8559e34be0033f02c8fa6892
kackur <44867189+kackur@users.noreply.github.com>
2024-10-17 21:10:29 +0200

    Delete youtube.js

--- youtube.js
+++
@@ -1,8 +0,0 @@
-// Initiera Vimeo-spelaren med Plyr
-const vimeoPlayer = new Plyr('#vimeo-player', {
-  controls: ['play', 'progress', 'current-time', 'mute', 'fullscreen'], // Plyrs kontroller
-  disableContextMenu: true, // Inaktivera högerklicksmenyer
-  fullscreen: {
-    enabled: true, // Tillåt fullskärm
-  },
-});
